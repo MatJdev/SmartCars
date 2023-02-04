ce688d495d791437b2f323f9e68a8138f54fe1d3
MatJDev <matiasj.a3@gmail.com>
2023-02-04 01:43:46 +0100

    Add welcome screen

--- app/src/main/java/com/jetpackcompose/smartcars/ui/login/ui/LoginScreen.kt
+++ app/src/main/java/com/jetpackcompose/smartcars/ui/login/ui/LoginScreen.kt
@@ -8,25 +8,37 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.jetpackcompose.smartcars.R
 
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LoginScreen() {
     Box {
-        Image(painter = painterResource(id = R.drawable.loginbg), contentDescription = "",
-        modifier = Modifier.fillMaxWidth())
+        //Modifier.paint(painterResource(id = R.drawable.loginbg)) -> Imagen de Fondo
+        //Image(painter = painterResource(id = R.drawable.loginbg), contentDescription = "",
+        //modifier = Modifier.fillMaxWidth())
+        BackGround()
+        Logo()
+        Text()
         Row1()
         EmailPass()
         Btn()
@@ -36,11 +48,13 @@ fun LoginScreen() {
 
 @Composable
 fun Row1() {
-    Text(text = "Log in", modifier = Modifier.padding(top = 270.dp, start = 70.dp),
-        fontWeight = FontWeight.Bold
+    Text(text = "Log in", modifier = Modifier.padding(top = 330.dp, start = 70.dp),
+        fontWeight = FontWeight.ExtraBold,
+        style = TextStyle(textDecoration = TextDecoration.Underline,
+        fontSize = 20.sp)
     )
-    Text(text = "Sign up", modifier = Modifier.padding(top = 270.dp, start = 300.dp),
-        fontWeight = FontWeight.Bold)
+    Text(text = "Sign up", modifier = Modifier.padding(top = 330.dp, start = 260.dp),
+        fontWeight = FontWeight.Bold, fontSize = 20.sp)
 }
 
 @Composable
@@ -48,25 +62,37 @@ fun EmailPass() {
     var textEmail by remember {mutableStateOf("")}
     var textPass by remember {mutableStateOf("")}
 
-    Text(text = "Email", modifier = Modifier.padding(top = 320.dp, start = 70.dp))
-    OutlinedTextField(value = textEmail, onValueChange = {},
-    modifier = Modifier.padding(top = 350.dp, start = 60.dp)
-        .border(BorderStroke(width = 3.dp, color = Color.DarkGray))
-        .background(color = Color.White))
+    Text(text = "Email", modifier = Modifier.padding(top = 390.dp, start = 70.dp))
+    TextField(value = textEmail, onValueChange = { textEmail = it },
+        modifier = Modifier
+            .padding(top = 420.dp, start = 60.dp)
+            .border(
+                width = 2.dp,
+                brush = Brush.horizontalGradient(listOf(Color.DarkGray, Color.DarkGray)),
+                shape = RoundedCornerShape(12.dp)
+            )
+    )
 
-    Text(text = "Password", modifier = Modifier.padding(top = 420.dp, start = 70.dp))
-    OutlinedTextField(value = textPass, onValueChange = {},
-        modifier = Modifier.padding(top = 450.dp, start = 60.dp).background(color = Color.White))
+    Text(text = "Password", modifier = Modifier.padding(top = 490.dp, start = 70.dp))
+    OutlinedTextField(value = textPass, onValueChange = { textPass = it},
+        modifier = Modifier
+            .padding(top = 520.dp, start = 60.dp)
+            .background(color = Color.White)
+            .border(
+                width = 2.dp,
+                brush = Brush.horizontalGradient(listOf(Color.DarkGray, Color.DarkGray)),
+                shape = RoundedCornerShape(12.dp)
+            ))
 }
 
 @Composable
 fun Btn() {
-    Column(Modifier.padding(top = 520.dp, start = 130.dp)) {
+    Column(Modifier.padding(top = 620.dp, start = 130.dp)) {
         Button(onClick = {
 
         },
             colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.DarkGray,
+                backgroundColor = Color(0xFF2C2B34),
                 contentColor = Color.White
             ), modifier = Modifier.width(140.dp)){
             Text(text="Login")
@@ -74,10 +100,59 @@ fun Btn() {
 
         TextButton(onClick = { },
             colors = ButtonDefaults.textButtonColors(
-                contentColor = Color.DarkGray
+                contentColor = Color(0xFF2C2B34)
             ), modifier = Modifier.padding(start = 10.dp)
         ) {
             Text(text = "Or sign up here")
+        }
+    }
+
+}
+
+@Composable
+fun Text() {
+    Text( text = "Hey!\nWelcome Back",
+        modifier = Modifier.padding(top = 190.dp, start = 40.dp),
+    color = Color.White,
+    fontSize = 35.sp)
+}
+
+@Composable
+fun Logo() {
+    Image(painter = painterResource(id = R.drawable.cocheelectrico), contentDescription = "",
+        modifier = Modifier
+            .width(270.dp)
+            .padding(start = 140.dp, top = 60.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BackGround() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()){
+
+        val (BoxLight, BoxDark) = createRefs()
+        val guiaHorizontalTop = createGuidelineFromTop(0.4f)
+
+        Box(modifier = Modifier
+            .background(color = Color(0xFF2C2B34))
+            .constrainAs(BoxDark) {}
+            .fillMaxSize()){
+
+        }
+        Box(
+            modifier = Modifier
+                .size(width = 400.dp, height = 800.dp)
+                .clip(
+                    shape = RoundedCornerShape(
+                        topStart = 40.dp,
+                        topEnd = 40.dp,
+                        bottomStart = 0.dp,
+                        bottomEnd = 0.dp
+                    )
+                )
+                .background(color = Color(0xFFF3F3F3))
+                .constrainAs(BoxLight) { top.linkTo(guiaHorizontalTop) }
+        ) {
         }
     }
 
---
+++ app/src/main/java/com/jetpackcompose/smartcars/ui/welcome/ui/WelcomeScreen.kt
@@ -0,0 +1,70 @@
+package com.jetpackcompose.smartcars.ui.welcome.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.jetpackcompose.smartcars.R
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun WelcomeScreen() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+        val (foto, BoxDark, btn, text) = createRefs()
+        val guiaHorizontalTop = createGuidelineFromTop(0.2f)
+        val guiaVerticalTop = createGuidelineFromStart(0.7f)
+
+        Box(modifier = Modifier
+            .background(color = Color(0xFF2C2B34))
+            .constrainAs(BoxDark) {}
+            .fillMaxSize()) {
+
+        }
+
+
+        Image(painter = painterResource(id = R.drawable.tesla1), contentDescription = ""
+            , modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(foto) {
+                    end.linkTo(guiaVerticalTop)
+                    top.linkTo(guiaHorizontalTop)
+                }
+        )
+
+        Text( text = "Premiums Cars.\nEnjoy the luxury",
+            modifier = Modifier.constrainAs(text){
+                top.linkTo(foto.bottom, margin = 20.dp)
+                start.linkTo(parent.start, margin = 30.dp)
+            },
+            color = Color.White,
+            fontSize = 35.sp)
+
+        Button(onClick = {
+
+        },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.White,
+                contentColor = Color(0xFF2C2B34)
+            ), modifier = Modifier.width(200.dp)
+                .constrainAs(btn){
+                    top.linkTo(text.bottom, margin = 100.dp)
+                    start.linkTo(parent.start, margin = 100.dp)
+                }){
+            Text(text="Let's Go", fontSize = 20.sp)
+        }
+
+    }
+
+}
