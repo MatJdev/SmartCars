6ac9af6e64de491954436e0d31736e882e5bd0a6
MatJDev <matiasj.a3@gmail.com>
2023-02-16 23:20:01 +0100

    Update HomeScreen

--- app/src/main/java/com/jetpackcompose/smartcars/ui/home/ui/HomeScreen.kt
+++ app/src/main/java/com/jetpackcompose/smartcars/ui/home/ui/HomeScreen.kt
@@ -4,21 +4,35 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BatteryChargingFull
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.NearMe
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.jetpackcompose.smartcars.R
 import com.jetpackcompose.smartcars.navigation.AppScreens
 
@@ -73,14 +87,39 @@ fun info() {
 fun nearestCar(){
     Card(
         elevation = 10.dp,
-        border = BorderStroke(1.dp, Color.LightGray),
+        border = BorderStroke(1.dp, Color(0xFFE6E6E6)),
         modifier = Modifier
             .padding(start = 30.dp, end = 30.dp)
             .width(340.dp)
-            .height(200.dp),
-        backgroundColor = Color.LightGray
+            .height(230.dp),
+        backgroundColor = Color(0xFFE6E6E6),
+        shape = RoundedCornerShape(20.dp)
     ) {
-        Text(text = "Nearest Car", modifier = Modifier.padding(20.dp))
+        Text(text = "Nearest Car", modifier = Modifier.padding(20.dp), fontWeight = FontWeight.Light)
+        AsyncImage(
+            model = "https://assets.stickpng.com/images/580b585b2edbce24c47b2ccc.png",
+            contentDescription = null,
+        )
+        //https://assets.stickpng.com/images/580b585b2edbce24c47b2ccc.png
+        //https://www.pngmart.com/files/21/Tesla-Car-Transparent-Background.png
+        Text(text = "Tesla Model S", modifier = Modifier.padding(top = 165.dp, start = 20.dp),
+            fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color(0xFF2C2B34))
+        Row(Modifier.padding(top = 195.dp)){
+            Icon(
+                Icons.Outlined.NearMe,
+                contentDescription = "",
+                Modifier.padding(start = 20.dp).size(18.dp)
+            )
+            Text(text = "< 2km", modifier = Modifier.padding(start = 2.dp))
+            Icon(
+                Icons.Outlined.BatteryChargingFull,
+                contentDescription = "",
+                Modifier.padding(start = 30.dp).size(18.dp)
+            )
+            Text(text = "85%", modifier = Modifier.padding(start = 2.dp))
+            Text(text = "10€/h", modifier = Modifier.padding(start = 90.dp))
+        }
+
     }
 }
 
@@ -89,27 +128,43 @@ fun profileMap(){
     Row(Modifier.padding(top = 20.dp, bottom = 20.dp)) {
         Card(
             elevation = 10.dp,
-            border = BorderStroke(1.dp, Color.LightGray),
+            border = BorderStroke(1.dp, Color(0xFFE6E6E6)),
             modifier = Modifier
                 .padding(start = 30.dp, end = 20.dp)
                 .width(150.dp)
                 .height(150.dp),
-            backgroundColor = Color.LightGray
+            backgroundColor = Color(0xFFE6E6E6),
+            shape = RoundedCornerShape(20.dp)
         ) {
-            Text(text = "Name", modifier = Modifier.padding(20.dp))
-            Text(text = "150 €", modifier = Modifier.padding(50.dp))
+            Box(Modifier.padding(top = 20.dp, start = 35.dp)){
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://www.pngitem.com/pimgs/m/551-5510463_default-user-image-png-transparent-png.png")
+                        .transformations(CircleCropTransformation())
+                        .build(),
+                    contentDescription = null,
+                    modifier = Modifier.clip(CircleShape).size(80.dp)
+                )
+            }
+            Text(text = "Name", textAlign = TextAlign.Center, modifier = Modifier.padding(top = 100.dp))
+            Text(text = "150 €", textAlign = TextAlign.Center, modifier = Modifier.padding(top = 120.dp))
         }
 
         Card(
             elevation = 10.dp,
-            border = BorderStroke(1.dp, Color.LightGray),
+            border = BorderStroke(1.dp, Color(0xFFE6E6E6)),
             modifier = Modifier
                 .padding(start = 10.dp, end = 30.dp)
                 .width(150.dp)
                 .height(150.dp),
-            backgroundColor = Color.LightGray
+            backgroundColor = Color(0xFFE6E6E6),
+            shape = RoundedCornerShape(20.dp)
         ) {
-            Text(text = "Map", modifier = Modifier.padding(20.dp))
+            //Text(text = "Map", modifier = Modifier.padding(20.dp))
+            AsyncImage(
+                model = "https://img.freepik.com/premium-vector/street-map-with-pin-routes_23-2147622544.jpg",
+                contentDescription = null,
+            )
         }
     }
 }
@@ -123,7 +178,8 @@ fun moreCars(){
             .padding(start = 30.dp, end = 30.dp)
             .width(340.dp)
             .height(200.dp),
-        backgroundColor = Color(0xFF2C2B34)
+        backgroundColor = Color(0xFF2C2B34),
+        shape = RoundedCornerShape(20.dp)
     ) {
         Text(text = "More Cars", modifier = Modifier.padding(20.dp), color = Color.White)
     }
@@ -135,10 +191,11 @@ fun menuBar(){
         elevation = 10.dp,
         border = BorderStroke(1.dp, Color.LightGray),
         modifier = Modifier
-            .padding(top = 40.dp, start = 30.dp, end = 30.dp)
+            .padding(top = 20.dp, start = 30.dp, end = 30.dp)
             .width(340.dp)
             .height(60.dp),
-        backgroundColor = Color(0xFF2C2B34)
+        backgroundColor = Color(0xFF2C2B34),
+        shape = RoundedCornerShape(20.dp)
     ) {
 
     }
