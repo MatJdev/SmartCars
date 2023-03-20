6310ee4bf7818b78fa15b87c6ae0ca18512bd4ca
Pepe <j.ortega@cesjuanpablosegundocadiz.es>
2023-03-20 16:38:36 +0100

    Recycler View data loaded

--- app/src/main/java/com/jetpackcompose/smartcars/ui/RV/Recycler.kt
+++ app/src/main/java/com/jetpackcompose/smartcars/ui/RV/Recycler.kt
@@ -1,5 +1,7 @@
 package com.jetpackcompose.smartcars.ui.RV
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
@@ -17,6 +19,7 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,18 +30,34 @@ import androidx.compose.ui.res.painterResource
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
 import com.jetpackcompose.smartcars.R
 import com.jetpackcompose.smartcars.ui.RV.model.Coche
+import com.jetpackcompose.smartcars.ui.data.DataViewModel
+import com.jetpackcompose.smartcars.ui.data.getDataFromFireStore
+import com.jetpackcompose.smartcars.ui.data.model.Car
+import com.jetpackcompose.smartcars.ui.map.ui.setValue
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 //@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun RvScreen() {
+fun RvScreen(dataViewModel: DataViewModel = viewModel()) {
+
+    var getData = dataViewModel.state.value
+    var imgCar =
+        rememberSaveable { mutableStateOf("https://www.pngplay.com/wp-content/uploads/13/2018-Tesla-Model-S-Transparent-PNG.png") }
+    var marcaCar = rememberSaveable { mutableStateOf("Tesla") }
+    var modeloCar = rememberSaveable { mutableStateOf("Model S") }
 
     val context = LocalContext.current
     val rvState = rememberLazyListState()
     val coroutineScope = rememberCoroutineScope()
+
 
     LazyColumn(
         state = rvState,
@@ -46,77 +65,148 @@ fun RvScreen() {
         modifier = Modifier.background(Color.White),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        items(getCoches()) { Coche ->
-            ItemCoche(Coche = Coche) {
+        items(getData) { Car ->
+            ItemCoche(Car = Car!!) {
                 Toast.makeText(context, it.marca, Toast.LENGTH_LONG).show()
             }
         }
     }
-    val showbutton by remember {
-        derivedStateOf {
-            rvState.firstVisibleItemIndex > 1
-        }
-    }
-    if (showbutton) {
-        Button(
-            onClick = { coroutineScope.launch { rvState.animateScrollToItem(9) } },
-            modifier = Modifier.padding(15.dp)
-        ) {
-            Text(text = "Bajar")
-        }
-    }
+
+//    val showbutton by remember {
+//        derivedStateOf {
+//            rvState.firstVisibleItemIndex > 1
+//        }
+//    }
+//    if (showbutton) {
+//        Button(
+//            onClick = { coroutineScope.launch { rvState.animateScrollToItem(9) } },
+//            modifier = Modifier.padding(15.dp)
+//        ) {
+//            Text(text = "Bajar")
+//        }
+//    }
 
 }
 
-fun getCoches(): List<Coche> {
+fun getCoches(): List<Car> {
+
     return listOf(
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
-        Coche("Tesla", "S", "Electrico", R.drawable.tesla1),
+        Car(),
+        Car(),
+        Car(),
+        Car(),
+        Car(),
+        Car(),
+
 
     )
 }
 
+
+
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun ItemCoche(Coche: Coche, onItemSelected: (Coche) -> Unit) {
+fun ItemCoche(Car: Car, dataViewModel: DataViewModel = viewModel(), onItemSelected: (Car) -> Unit) {
+
+
+    var getData = dataViewModel.state.value
+//
+//
+    var imgCar = rememberSaveable { mutableStateOf("https://www.pngplay.com/wp-content/uploads/13/2018-Tesla-Model-S-Transparent-PNG.png") }
+    var marcaCar = rememberSaveable { mutableStateOf("Tesla") }
+    var modeloCar = rememberSaveable { mutableStateOf("Model S") }
+
+
+    imgCar.setValue(Car.img)
+    marcaCar.setValue(Car.marca)
+    modeloCar.setValue(Car.modelo)
+
+//
+//    val scope = rememberCoroutineScope()
+//    scope.launch {
+//
+//        while (getData == null || getData.isEmpty()) {
+//            delay(200L) // esperar un segundo
+//        }
+//        // ejecutar código aquí
+//        for (item in getData) {
+//            modeloCar.setValue(item!!.modelo)
+//            marcaCar.setValue(item!!.marca)
+//            imgCar.setValue(item!!.img)
+//
+//            val entrees = mutableListOf<String>()
+//            entrees.add(marcaCar.value)
+//            entrees.add(modeloCar.value)
+//            Log.i("Prueba datos" , entrees.toString())
+//        }
+//    }
+//    Log.i("Datos en recycler", marcaCar.toString())
+
+
+//    val scope = rememberCoroutineScope()
+//    scope.launch {
+//
+//        while (getData == null || getData.isEmpty()) {
+//            delay(200L) // esperar un segundo
+//        }
+//        // ejecutar código aquí
+//
+//        imgCar.setValue(getData[0]!!.img)
+//        marcaCar.setValue(getData[0]!!.marca)
+//        modeloCar.setValue(getData[0]!!.modelo)
+//
+//        Log.i("Datos en recycler", marcaCar.toString())
+//
+//    }
+
+
     Card(
         modifier = Modifier
             .padding(horizontal = 8.dp, vertical = 8.dp)
             .fillMaxWidth(),
         elevation = 2.dp,
-        backgroundColor =  Color(0xFF2C2B34),
+        backgroundColor = Color(0xFF2C2B34),
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
-        border = BorderStroke(6.dp,Color.Black),
-
+        border = BorderStroke(6.dp, Color.Black),
 
         ) {
         Row {
-            Image(
-                painter = painterResource(id = Coche.imagen),
+//            Image(
+//                painter = painterResource(id = Car.img),
+//                contentDescription = null,
+//                contentScale = ContentScale.Fit,
+//                modifier = Modifier
+//                    .padding(8.dp)
+//                    .size(100.dp)
+//                    .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+//            )
+            AsyncImage(
                 contentDescription = null,
                 contentScale = ContentScale.Fit,
                 modifier = Modifier
                     .padding(8.dp)
                     .size(100.dp)
-                    .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
-            )
+                    .clip(RoundedCornerShape(corner = CornerSize(16.dp))),
+                model = imgCar.value,
+            );
             Column(
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth()
                     .align(Alignment.CenterVertically)
             ) {
-                Text(text = Coche.marca, style = MaterialTheme.typography.h5,color = Color.White)
-                Text(text = Coche.modelo, style = MaterialTheme.typography.subtitle1,color = Color.White)
-                Text(text = Coche.tipo, style = MaterialTheme.typography.subtitle2,color = Color.White)
+
+                Text(
+                    text = marcaCar.value,
+                    style = MaterialTheme.typography.h5,
+                    color = Color.White
+                )
+                Text(
+                    text = modeloCar.value,
+                    style = MaterialTheme.typography.subtitle1,
+                    color = Color.White
+                )
+
             }
         }
     }
--- app/src/main/java/com/jetpackcompose/smartcars/ui/account/ui/AccountScreen.kt
+++ app/src/main/java/com/jetpackcompose/smartcars/ui/account/ui/AccountScreen.kt
@@ -1,12 +1,30 @@
 package com.jetpackcompose.smartcars.ui.account.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.FabPosition
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.jetpackcompose.smartcars.ui.home.ui.BottomNavigationBar
 import com.jetpackcompose.smartcars.ui.home.ui.MyFab
 
@@ -21,16 +39,171 @@ fun AccountScreen(navController: NavController) {
 fun Scaffold(navController: NavController) {
     val scaffoldState = rememberScaffoldState()
 
-    androidx.compose.material.Scaffold(
+    Scaffold(
         scaffoldState = scaffoldState,
         bottomBar = { BottomNavigationBar("account_screen", navController) },
         floatingActionButton = { MyFab() },
         floatingActionButtonPosition = FabPosition.Center,
         isFloatingActionButtonDocked = true
     ) {
+            UserCard()
+//            Spacer(modifier = Modifier
+//                .height(10.dp)
+//                .border(1.dp, color = Color.Black))
+
+            Divider(color = Color.Black, thickness = 1.dp)
+            DownCards()
+        }
+    }
+
+@Composable
+fun DownCards(){
+    Row(Modifier.padding(top = 400.dp, bottom = 20.dp)){
         Column {
+            Card(
+                modifier = Modifier
+                    .padding(start = 10.dp, end = 10.dp)
+                    .width(500.dp)
+                    .height(50.dp),
+                elevation = 2.dp,
+                backgroundColor = Color(0xFFE6E6E6),
+                shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+
+                border = BorderStroke(1.dp, Color.Black),
+            ){
+                Text(
+                    text = "Credit : ",
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Start,
+                    color = Color.Black,
+                    modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+                )
+            }
+            Spacer(modifier = Modifier
+                .height(20.dp))
+            Card(
+                modifier = Modifier
+                    .padding(start = 10.dp, end = 10.dp)
+                    .width(500.dp)
+                    .height(50.dp),
+                elevation = 2.dp,
+                backgroundColor = Color(0xFFE6E6E6),
+                shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+
+                border = BorderStroke(1.dp, Color.Black),
+            ){
+                Text(
+                    text = "Id MetaMask : ",
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Start,
+                    color = Color.Black,
+                    modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+                )
+            }
+            Spacer(modifier = Modifier
+                .height(20.dp))
+            Card(
+                modifier = Modifier
+                    .padding(start = 10.dp, end = 10.dp)
+                    .width(500.dp)
+                    .height(50.dp),
+                elevation = 2.dp,
+                backgroundColor = Color(0xFFE6E6E6),
+                shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+
+                border = BorderStroke(1.dp, Color.Black),
+            ){
+                Text(
+                    text = "Reset Password",
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Center,
+                    color = Color.Black,
+                    modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+                )
+            }
+            Spacer(modifier = Modifier
+                .height(20.dp))
+            Card(
+                modifier = Modifier
+//                    .padding(start = 100.dp, end = 50.dp)
+                    .width(100.dp)
+                    .height(50.dp)
+                    .align(Alignment.CenterHorizontally),
+                elevation = 2.dp,
+                backgroundColor = Color(0xFF303030),
+                shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+                border = BorderStroke(1.dp, Color.Black),
+
+            ){
+                Text(
+                    text = "Log out",
+                    style = MaterialTheme.typography.h5,
+                    textAlign = TextAlign.Center,
+                    color = Color.White
+                )
+            }
+
 
         }
     }
 
+}
+
+@Composable
+fun UserCard() {
+
+    Column {
+        Card(
+            elevation = 10.dp,
+            border = BorderStroke(1.dp, Color(0xFFE6E6E6)),
+            modifier = Modifier
+                .padding(start = 40.dp, end = 40.dp, top = 20.dp)
+                .width(300.dp)
+                .height(300.dp),
+            backgroundColor = Color(0xFFE6E6E6),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            Box(Modifier.align(Alignment.CenterHorizontally)) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://www.pngitem.com/pimgs/m/551-5510463_default-user-image-png-transparent-png.png")
+                        .transformations(CircleCropTransformation())
+                        .build(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(100.dp)
+                        .align(alignment = Alignment.Center)
+                )
+
+            }
+            Text(
+                text = "Name",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 200.dp)
+            )
+            Text(
+                text = "150 €",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 220.dp)
+            )
+            Text(
+                text = "Email",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 240.dp)
+            )
+            Canvas(modifier = Modifier
+                .height(10.dp).width(300.dp)
+                .wrapContentSize()) {
+                drawLine(
+                    color = Color.LightGray,
+                    start = Offset(x = -400f, y = 20f),
+                    end = Offset(x = 400f, y = 20f),
+                    strokeWidth = 2.dp.toPx(),
+                    cap = StrokeCap.Round
+                )}
+
+        }
+
+    }
 }
